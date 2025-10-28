407a34b39ddb32d70e142dabb8650af2e998c31e
Figueiredo Sebastiao <47941@alunos.upt.pt>
2025-10-28 17:02:40 +0000

    nova <branch

--- src/main/java/upt/pl/equipa2/Utilizador.java
+++ src/main/java/upt/pl/equipa2/Utilizador.java
@@ -1,79 +1,65 @@
 package upt.pl.equipa2;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "utilizador")
 public class Utilizador {
-	private String idUtilizador;
-	private String nome;
-	private int idade;
-	private String password;
-	
-	
-	
-	public Utilizador(String idUtilizador, String nome, int idade, String password) {
-		this.idUtilizador = idUtilizador;
-		this.nome = nome;
-		this.idade = idade;
-		this.password = password;
-	}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_utilizador")
+    private int idUtilizador;
 
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
 
-	public String getIdUtilizador() {
-		return idUtilizador;
-	}
+    @Column(name = "idade", nullable = false)
+    private int idade;
 
+    @Column(name = "pass", nullable = false, length = 7)
+    private String password;
 
+    // Construtor vazio (obrigatório pelo JPA)
+    public Utilizador() {
+    }
 
-	public void setIdUtilizador(String idUtilizador) {
-		this.idUtilizador = idUtilizador;
-	}
+    // Getters e setters
+    public int getIdUtilizador() {
+        return idUtilizador;
+    }
 
+    public void setIdUtilizador(int idUtilizador) {
+        this.idUtilizador = idUtilizador;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public int getIdade() {
+        return idade;
+    }
 
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-
-	public int getIdade() {
-		return idade;
-	}
-
-
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-
-
-	public String getPassword() {
-		return password;
-	}
-
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Utilizador [idUtilizador=" + idUtilizador + ", nome=" + nome + ", idade=" + idade + ", password="
-				+ password + "]";
-	}
-	
-	
-	
-	
-	
-	
+    @Override
+    public String toString() {
+        return "Utilizador [idUtilizador=" + idUtilizador + ", nome=" + nome +
+               ", idade=" + idade + ", password=" + password + "]";
+    }
 }
