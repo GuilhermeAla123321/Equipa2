5b8d15e81c97b3411ae1cb941d86c3a25c0069f2
guilh <guilh@LAPTOP-N8GLF45M>
2025-10-28 20:51:22 +0000

    erro

--- src/main/java/upt/pl/equipa2/Main.java
+++ src/main/java/upt/pl/equipa2/Main.java
@@ -4,24 +4,4 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory; 
 import org.hibernate.cfg.Configuration; 
 
-public class Main { 
-    public static void main(String[] args) { 
-        // Build SessionFactory from configuration file 
-        SessionFactory factory = new 
-Configuration().configure().buildSessionFactory(); 
-
-        // Open session 
-        try (Session session = factory.openSession()) { 
-            session.beginTransaction(); 
-            Programming Laboratory 
-
-
-            session.getTransaction().commit(); 
-
-            System.out.println(" Students saved successfully!"); 
-        } finally { 
-            factory.close(); 
-        } 
-    } 
-
-} 
+public class Main {
