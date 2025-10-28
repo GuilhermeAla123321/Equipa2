1ab26e266abbb3b839a40a4bcabaf8d05a6b03fb
guga2 <guga2@192.168.1.179>
2025-10-28 18:41:42 +0000

    Merge branch 'master' of https://github.com/GuilhermeAla123321/Equipa2.git

--- src/main/java/upt/pl/equipa2/Docente.java
+++ src/main/java/upt/pl/equipa2/Docente.java
@@ -1,12 +1,29 @@
 package upt.pl.equipa2;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "docente")
 public class Docente extends Utilizador {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_docente")
+    private int id_docente;
+
+    @Column(name = "emailIns", nullable = false, length = 100)
     private String emailIns;
 
-    public Docente(int idUtilizador, String nome, int idade, String password, String emailIns) {
-        super(idUtilizador, nome, idade, password);
-        this.emailIns = emailIns;
+    public Docente() {
+    }
+
+  
+    public int getId_docente() {
+        return id_docente;
+    }
+
+    public void setId_docente(int id_docente) {
+        this.id_docente = id_docente;
     }
 
     public String getEmailIns() {
