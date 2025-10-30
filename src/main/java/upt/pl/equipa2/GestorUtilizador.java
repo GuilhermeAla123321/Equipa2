382d79c2b5679e0e30de71e4021763aa671efcd1
Utilizador <Utilizador@Beast>
2025-10-30 18:12:42 +0000

    loginUtilizador()

--- src/main/java/upt/pl/equipa2/GestorUtilizador.java
+++ src/main/java/upt/pl/equipa2/GestorUtilizador.java
@@ -5,23 +5,43 @@ import org.hibernate.Session;
 public class GestorUtilizador {
 	
 	
-public void registarUtilizador(String nome, int idade, String password) {
-    Session session = HibernateUtil.getSessionFactory().openSession();
-    session.beginTransaction();
+	public void registarUtilizador(String nome, int idade, String password) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
     
-    Utilizador novoUtilizador = new Utilizador();
-    novoUtilizador.setNome(nome);
-    novoUtilizador.setIdade(idade);
-    novoUtilizador.setPassword(password);
+		Utilizador novoUtilizador = new Utilizador();
+		novoUtilizador.setNome(nome);
+		novoUtilizador.setIdade(idade);
+		novoUtilizador.setPassword(password);
     
     
-    session.persist(novoUtilizador);
-    session.getTransaction().commit();
-    session.close();
+		session.persist(novoUtilizador);
+		session.getTransaction().commit();
+		session.close();
 
-    System.out.println("Utilizador registado!");
+		System.out.println("Utilizador registado!");
     
-	}   
+	}
+	
+	 public void loginUtilizador(int id, String password) {
+	        Session session = HibernateUtil.getSessionFactory().openSession();
+	        session.beginTransaction();
+
+
+	        Utilizador utilizador = session.get(Utilizador.class, id);
+
+	        if (utilizador == null) {
+	            System.out.println("Utilizador não encontrado!");
+	        } else if (utilizador.getPassword().equals(password)) {
+	            System.out.println("Login efetuado com sucesso! Bem-vindo, " + utilizador.getNome() + "!");
+	        } else {
+	            System.out.println("Password incorreta!");
+	        }
+
+	        session.getTransaction().commit();
+	        session.close();
+	    }
+
 }
 	
 	
